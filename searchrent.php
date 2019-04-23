1ea8820f7f5b66654c38c8238e63358feab7c788
jc4va <31323449+jc4va@users.noreply.github.com>
2019-04-23 19:47:34 -0400

    rent

--- searchrent.php
+++ searchrent.php
@@ -6,7 +6,7 @@
         $stmt = $db->stmt_init();
 
         if($stmt->prepare("select residency_address, rent, num_possible_residents, num_current_residents, num_room, num_bed, landlord_name, phone
-        from residency natural join own natural join landlord where rent < ?") or die(mysqli_error($db))) {
+        from residency natural join own natural join landlord where rent < ? order by rent") or die(mysqli_error($db))) {
                 $searchString = $_GET['searchrent'];
                 $stmt->bind_param(s, $searchString);
                 $stmt->execute();
